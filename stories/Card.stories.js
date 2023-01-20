3149c237b0fa243fe14507d4bde0b896d6507590
Hugo Miranda <hm@signifly.com>
2023-01-20 12:27:20 -0500

    Card Image

--- stories/Card.stories.js
+++ stories/Card.stories.js
@@ -21,7 +21,7 @@ const Template = ({ label, ...args }) => {
 export const Large = Template.bind({});
 Large.args = {
   size: 'large',
-  imgSrc: '/',
+  imgSrc: 'https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80',
   title: 'Title',
   subtitle: 'Subtitle',
 };
@@ -29,7 +29,7 @@ Large.args = {
 export const Medium = Template.bind({});
 Medium.args = {
   size: 'medium',
-  imgSrc: '/',
+  imgSrc: 'https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80',
   title: 'Title',
   subtitle: 'Subtitle',
 };
@@ -37,7 +37,7 @@ Medium.args = {
 export const Small = Template.bind({});
 Small.args = {
   size: 'small',
-  imgSrc: '/',
+  imgSrc: 'https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80',
   title: 'Title',
   subtitle: 'Subtitle',
 };
--- stories/Image.stories.js
+++ stories/Image.stories.js
@@ -4,7 +4,16 @@ export default {
   title: 'Components/Image',
   parameters: {
     layout: 'fullscreen'
-  },
+  }
 }
 
-export const Image = (args) => createImage(args)
+// export const Image = (args) => createImage(args)
+
+const Template = (args) => {
+  return createImage(args)
+}
+
+export const Image = Template.bind({})
+Image.args = {
+  src: 'https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80'
+}
