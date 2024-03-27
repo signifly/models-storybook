e2e2d980a73d6a7f09caee180f6f427b5722c0a0
Philippe Trudel <p.trudel1996@gmail.com>
2024-03-27 17:50:55 -0400

    feat: input field

---
+++ stories/TextInput.js
@@ -0,0 +1,33 @@
+import { strToDom, domToStr } from "../utilities/dom";
+
+export const createTextInput = ({
+    name,
+    placeholder,
+    label,
+    hint,
+    isError,
+    disabled = false,
+    type = "text"
+}) => {
+
+    // class creator with modifier
+    const c = (modifier) => `sb-text-input__${modifier}`
+
+    const classNames = {
+        layout: `${c('layout')}`,
+        wrapper: `${c('wrapper')} ${disabled ? c('wrapper--disabled') : ''} ${isError ? c('wrapper--error') : ''}`,
+        label: `${c('label')}  ${disabled ? c('label--disabled') : ''}`,
+        input: `${c('input')} ${disabled ? c('input--disabled') : ''} ${isError ? c('input--error') : ''}`,
+        hint: `${c('hint')} ${isError ? c('hint--error') : ''}`
+    }
+
+    return strToDom(`
+        <div class="${classNames.layout}" >
+            <div class="${classNames.wrapper}" >
+                <label class="${classNames.label}" for="${name}" >${label}</label>
+                <input class="${classNames.input}" name="${name}" placeholder="${placeholder}" type="${type}" ${disabled ? "disabled" : ''} />
+            </div>
+            ${hint && `<p class="${classNames.hint}" >${hint}</p>`}
+        </div>
+    `)
+}
